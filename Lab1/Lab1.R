c0b399615c9611501a78711b1d9eedba54e3ab3b
unknown <zliu15@4213PC07.stat.ad.ncsu.edu>
2015-08-25 22:12:23 -0400

    'Lab1'

--- Lab1/Lab1.R
+++ Lab1/Lab1.R
@@ -16,7 +16,7 @@ mean(body$age) # or mean(body[,1])
 var_height <- var(body$height) # var(body[,3])
 sqrt(var(body$weight))
 cor(body$height,body$weight)
-sqrt(var_height/nrow(body))
+sqrt(var_height/nrow(body)) # key to Q6
 males_gr <- body[body$gender=="Male",]
 colnames(males_gr)
 mean(males_gr$height)
